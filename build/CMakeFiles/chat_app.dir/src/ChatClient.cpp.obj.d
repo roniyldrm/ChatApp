560f76392081e68ca08cb0497165e918539155f7
ergenbalik <roniyildirim872@gmail.com>
2025-07-01 16:54:55 +0300

    Fix: untrack ignored files

--- build/CMakeFiles/chat_app.dir/src/ChatClient.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/chat_app.dir/src/ChatClient.cpp.obj: \
- C:\Users\pc\Desktop\ChatApp\src\ChatClient.cpp
--- build/CMakeFiles/chat_app.dir/src/ChatServer.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/chat_app.dir/src/ChatServer.cpp.obj: \
- C:\Users\pc\Desktop\ChatApp\src\ChatServer.cpp
